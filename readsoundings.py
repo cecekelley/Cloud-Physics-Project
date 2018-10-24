1fe764b39c593f98add711f4b32805561035d6cc
cecekelley <mary.c.kelley@ttu.edu>
2018-10-24 13:28:41 -0500

    Made changes in sounding file

--- readsoundings.py
+++ readsoundings.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def parse_SPC(Sounding_for_CP_Project, skip_rows=6):
     dtype= [ ('p', float),       #pressure,mb
              ('z', float),       #altitude, m
@@ -6,5 +8,5 @@ def parse_SPC(Sounding_for_CP_Project, skip_rows=6):
              ('wind_dir', float), #wind direction, degrees
              ('wind_spd', float), #wind speed, knots
            ]
-    data = np.genfromtxt(filename, dtype=dtype, skip_header=skip_rows, delimiter=',')
+    data = np.genfromtxt(Sounding_for_CP_Project, dtype=dtype, skip_header=skip_rows, delimiter=',')
     return (data)
