9524391b56b2decc387828db7b2aaa31b7c8fd5f
cecekelley <mary.c.kelley@ttu.edu>
2018-10-25 13:05:30 -0500

    changes in RH

--- Bolton.py
+++ Bolton.py
@@ -28,8 +28,10 @@ def sat_vapor_temperature(e_s):
 
 def sat_mixing_ratio(p,T):
     e_s=sat_vapor_pressure(T)
-    sat_mix_rat= esp* (e_s/(p-e_s))
-    return(sat_mix_rat)
+    print(e_s)
+    sat_mixing_ratio= (esp*e_s)/(p-e_s)
+    return(sat_mixing_ratio)
+
 
 
 # In[5]:
@@ -46,6 +48,7 @@ print(mix_rat)
 
 
 def RH(T,p,w):
+    print("w=", w)
     R_H = (w/sat_mixing_ratio(p,T)) * 100
     return(R_H)
 
@@ -56,7 +59,9 @@ def RH(T,p,w):
 def T_LCL(T, p, w):
     R_H=RH(T, p, w)
     term_a= 1/((T+C_to_K)-55)
-    term_b= np.log(R_H/100)/2840
+    term_b= (np.log(R_H/100))/2840
+    print(R_H.min())
+    print(R_H.max())
     Temp_LCL= (1/(term_a-term_b))+55
     return(Temp_LCL)
 
@@ -73,7 +78,7 @@ def theta_dry(theta, p, p_0=1000.0):
 
 
 def pseudoeq_potential_T(T, p, w, p_0=1000.0):
-    Temp_LCL=T_LCL(T+C_to_K, p, w)
+    Temp_LCL=T_LCL(T, p, w)
     part_1=(T+C_to_K)*(p_0/p)**(0.2854*(1-0.28*(w*10**(-3))))
     part_2= (3.376/Temp_LCL)-0.00254
     part_3= (w*10**3)*(1+0.81*(w*10**-3))
@@ -86,6 +91,6 @@ def pseudoeq_potential_T(T, p, w, p_0=1000.0):
 
 def theta_ep_field(T, p, p_0=1000.0):
     w= sat_mixing_ratio(T,p)
-    pseudo_ep_field= pseudoeq_potential_T(T, p, w, p_0)
-    return(pseudo_ep_field)
+    theta_ep_field= pseudoeq_potential_T(T, p, w, p_0)
+    return(theta_ep_field)
 
